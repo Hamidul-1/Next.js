9823402af60f1dda6334c05bc47eb6102f82dda5
Hamidul-1 <hamidul786cse@gmail.com>
2022-10-30 12:57:40 +0600

    added

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,15 @@ import Link from "next/link";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRouter } from "next/router";
 
 function index() {
+  const router = useRouter();
+
+  const handleclick = () => {
+    router.replace("/about");
+  };
+
   return (
     <div>
       <h1>This is Home Page</h1>
@@ -13,7 +20,7 @@ function index() {
       <Link href="/profile">
         <h3>Go to Profile page</h3>
       </Link>
-      
+      <button onClick={handleclick}>from submitted</button>
     </div>
   )
 }
